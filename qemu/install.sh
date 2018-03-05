052431cbf1ff5e200c54214133901329f647a41a
Greg Richardson <greg@rightplaceconsulting.com>
2018-03-04 22:10:05 -0800

    wait for boot partition to be available to system

--- qemu/install.sh
+++ qemu/install.sh
@@ -6,17 +6,29 @@
 ## Greg Richardson
 #################################
 #set -x
-set -e
+#set -e
 
+# system partitions for the chroot
 dirs="dev proc sys"
+
+# generic, unique mount point for the chroot.
 NEW=/mnt/new-`date +%s`
+
+# hardcoded value for testing.
 DRIVE=/dev/sda
+
+# full path to partition, ie: /dev/sda1
 PARTITION="${DRIVE}1"
+
+# find the short name, ie: sda1
+PARTITION_NAME=`echo $PARTITION|sed -e 's|/| |g'|awk '{print $2'}`
+
+# hardcoded for now.  This could be bundled into the LIVE ISO long term; downloaded from the internet, etc.
 IMAGE=/media/ubuntu/backup/ubuntu1604.raw
 
 
 function install() {
-  echo -e "\n\nCurrent disks:"
+  echo -e "\n\nCurrent block devics:\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
   lsblk
 
   if [ -f $IMAGE ];then
@@ -31,6 +43,21 @@ function install() {
   # prepare the chroot env
   mkdir $NEW
   chmod 777 $NEW
+
+  # list out available disks and partitions before we loop / wait for $PARTITION_NAME to be available.
+  lsblk
+  while true; do
+    lsblk|grep $PARTITION_NAME
+    if [ $? -eq 0 ]; then
+      echo "Found the $PARTITION_NAME partition!"
+      break
+    fi
+
+    echo "Waiting for $PARTITION_NAME to be available..."
+    sleep 5
+  done
+
+  # mount now that partition is available
   mount $PARTITION $NEW
 
   # add system mounts for chroot env.
@@ -51,7 +78,7 @@ function install() {
 }
 
 
-# check runas user
+# check runas user; requires root.
 if [[ $UID != 0 ]]; then
   echo "Sorry, this script needs to be run as root / sudo"
   exit 2
